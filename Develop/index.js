4fc75c72503b86cc50d93c119750f1d0dd5e0b7c
Nouha Rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-08-10 16:04:28 +0100

    refeactor: added global requires and completing question array
    Authors:NR

---
+++ Develop/index.js
@@ -0,0 +1,45 @@
+
+// Global require's
+const inquirer= require('inquirer');
+const fs = require('fs');
+const generateMarkdown= require('./utils/generateMarkdown')
+
+// An array of objects detailing questions for user input
+function userInput() {
+    return inquirer.prompt([
+
+   
+
+    
+    // Project title name
+    {
+        type: 'input',
+        message: 'What project title would you like?',
+        name: 'project title',
+        // installed validator library with (npm install validator)
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input'
+    },
+    {
+        type: 'input'
+    }
+
+]);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
