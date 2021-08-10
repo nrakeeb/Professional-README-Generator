1e558d207e1c57160ce1b8d4fd675092bc477885
Nouha Rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-08-11 00:24:40 +0100

    refactor: completed question array
    Authors:NR

--- Develop/index.js
+++ Develop/index.js
@@ -27,11 +27,113 @@ function userInput() {
         }
     },
     {
-        type: 'input'
+        type:'input',
+        message: 'Write a brief description of your project: ',
+        name: 'description',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log ('Please enter a decription of your project to continue')
+                return false;
+            }
+        }
     },
     {
-        type: 'input'
-    }
+        type: 'input',
+        message: 'How do you install your project? (Required)',
+        name: 'installation',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide installation information to continue');
+                return false;
+            }    
+        } 
+
+    },
+    {
+        type: 'input',
+        message: 'How do you use this project? (Required)',
+        name: 'usage',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide information on how to use project');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        message: 'How do you test this project? (Required)',
+        name: 'testing',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('Please describe how to test this project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'How should people contribute to this project? (Required)',
+        name: 'contribution',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please provide information on how to contribute to the project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        message: 'Choose a license for your project (Required)',
+        name: 'licensing',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('Please pick a license for the project to continue');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github Repo',
+        message: 'Enter your GitHub Repository url (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Respository url address');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your deployed Web url (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your Web url address');
+                return false;
+            }
+        }
+    },
+    
 
 ]);
 
