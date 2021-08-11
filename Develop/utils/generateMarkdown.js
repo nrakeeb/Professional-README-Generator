eaacdb8c2abd9c67828127df468f93259e877f75
Nouha Rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-08-11 22:09:12 +0100

    refactor: created function to generate readme
    Authors: NR

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,51 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  [![license](https://img.shields.io/badge/license-${data.license}-green)](https://shields.io)
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+
+ 
+  ## 🔍 Description:
+  ${data.description}
+
+ 
+  ## 💾 Installation:
+  ${data.installation}
+
+  
+  ## 💻 Usage:
+  ${data.usage}
+
+
+  ## License:
+  [![license](https://img.shields.io/badge/license-${data.license}-green)](https://shields.io)
+  <br/>
+  This application is covered by the ${data.license} license.
+
+
+  ## 👪 Contribution:
+  ${data.contribution}
+
+
+  ## ✏️ Testing:
+  ${data.testing}
+
+
+  ## Questions:
+
+  - Github: 
+  Find me on GitHub:[${data.username}] (https://github.com/${data.username})
+  <br>
+  - Email: 
+  Email me on: ${data.email} 
+  
 `;
 }
 
